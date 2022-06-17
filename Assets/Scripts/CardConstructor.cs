01ed911b9c9aa116ba23db3669707144159b449d
DanielGuadard <erldand@gmail.com>
2022-06-17 18:36:40 -0300

    Compendium "download" card remake
    - Remake of the instantiation of the cards on Compendium, now they have their on status and id,
    - Added exit button on the initial screen
    -Added battle Power on the TeamSetItem Script and the compendium

--- Assets/Scripts/CardConstructor.cs
+++ Assets/Scripts/CardConstructor.cs
@@ -36,9 +36,7 @@ public class CardConstructor : MonoBehaviour
     }
 
     public CardList JsonData;
-    public GameObject JOHN_FANGOPref, CAPTAINPref,TedPref, NENRUNGPref, REZAULPref, GARRINCHAPref, LUKEPref, RAYPref, SIR_WESTLEYPref, LIZAPref, HANISPref, SARGEANT_HARTMANPref, RIDLEYPref, ARSELLAPref,
-    ROCUSPref, AARONPref, WANDERLEYPref, LOLAPref, TODDPref, JOHNNYPref, SOORYAPref, IRFANPref, DANILOPref, ZEPEREIRAPref, BOB_SAWPref, TISYAPref, SASHAPref, MEDHANSHPref, SIRILOPref, MAHESHPref,
-    RICHARDPref, EDPref, ADAMPref;
+    public GameObject ChampionPref;
 
     void Start()
     {
@@ -76,244 +74,21 @@ public class CardConstructor : MonoBehaviour
 
         foreach (Card card in JsonData.Deck)
         {
-            switch (card.name)
-            {
-                case "John Fango the Tactical Megalodon":
-                    GameObject.Instantiate(JOHN_FANGOPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 222;
-					card.brawl = 333;
-					card.cunning = 111;
-					card.idealPressure = 4;
-                    break;
-                case "Captain Costa the Man O'War":
-                    GameObject.Instantiate(CAPTAINPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 222;
-					card.brawl = 111;
-					card.cunning = 222;
-					card.idealPressure = 1;
-                    break;
-                case "Ted Righty the Boxer Whale":
-                    GameObject.Instantiate(TedPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 240;
-					card.brawl = 260;
-					card.cunning = 100;
-					card.idealPressure = 2;
-                    break;
-                case "Nenrung the Dragon":
-                    GameObject.Instantiate(NENRUNGPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 247;
-					card.brawl = 120;
-					card.cunning = 100;
-					card.idealPressure = 3;
-                    break;
-                case "Rezaul the master Octopus":
-                    GameObject.Instantiate(REZAULPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 75;
-					card.brawl = 150;
-					card.cunning = 233;
-					card.idealPressure = 1;
-                    break;
-                case "Garrincha the Tough Blue Lobster":
-                    GameObject.Instantiate(GARRINCHAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 220;
-					card.brawl = 220;
-					card.cunning = 51;
-					card.idealPressure = 2;
-                    break;
-                case "Luke the Turtle":
-                    GameObject.Instantiate(LUKEPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 150;
-					card.brawl = 261;
-					card.cunning = 85;
-					card.idealPressure = 2;
-                    break;
-                case "Ray the Whipping Manta":
-                    GameObject.Instantiate(RAYPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 181;
-					card.brawl = 264;
-					card.cunning = 55;
-					card.idealPressure = 3;
-                    break;
-                case "Sir Westley the Fencing Dolphin":
-                    GameObject.Instantiate(SIR_WESTLEYPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 183;
-					card.brawl = 75;
-					card.cunning = 110;
-					card.idealPressure = 1;
-                    break;
-                case "Liza the Carrier Crab":
-                    GameObject.Instantiate(LIZAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 120;
-					card.brawl = 193;
-					card.cunning = 78;
-					card.idealPressure = 3;
-                    break;
-                case "Hanis the Menacing Flatfish":
-                    GameObject.Instantiate(HANISPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 185;
-					card.brawl = 96;
-					card.cunning = 90;
-					card.idealPressure = 2;
-                    break;
-                case "Sargeant Hartman the Navy Seal":
-                    GameObject.Instantiate(SARGEANT_HARTMANPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 180;
-					card.brawl = 116;
-					card.cunning = 80;
-					card.idealPressure = 1;
-                    break;
-                case "Ridley the Olive Turtle":
-                    GameObject.Instantiate(RIDLEYPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 160;
-					card.brawl = 145;
-					card.cunning = 80;
-					card.idealPressure = 1;
-                    break;
-                case "Arsella the Sea Nettle":
-                    GameObject.Instantiate(ARSELLAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 85;
-					card.brawl = 115;
-					card.cunning = 169;
-					card.idealPressure = 4;
-                    break;
-                case "Rocus 'Shades' the Blue Shark":
-                    GameObject.Instantiate(ROCUSPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 100;
-					card.brawl = 203;
-					card.cunning = 90;
-					card.idealPressure = 2;
-                    break;
-                case "Aaron the Ramming Crab":
-                    GameObject.Instantiate(AARONPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 102;
-					card.brawl = 199;
-					card.cunning = 93;
-					card.idealPressure = 3;
-                    break;
-                case "Wanderley the Grappling Octopus":
-                    GameObject.Instantiate(WANDERLEYPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 93;
-					card.brawl = 100;
-					card.cunning = 171;
-					card.idealPressure = 1;
-                    break;
-                case "Lola the Buff Blowfish":
-                    GameObject.Instantiate(LOLAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 182;
-					card.brawl = 102;
-					card.cunning = 90;
-					card.idealPressure = 3;
-                    break;
-                case "Todd the Barber Eel":
-                    GameObject.Instantiate(TODDPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 150;
-					card.brawl = 79;
-					card.cunning = 93;
-					card.idealPressure = 4;
-                    break;
-                case "Jhonny the Sailor Squid":
-                    GameObject.Instantiate(JOHNNYPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 110;
-					card.brawl = 60;
-					card.cunning = 143;
-					card.idealPressure = 2;
-                    break;
-                case "Soorya the Needling Box Jellyfish":
-                    GameObject.Instantiate(SOORYAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 115;
-					card.brawl = 73;
-					card.cunning = 132;
-					card.idealPressure = 4;
-                    break;
-                case "Irfan the Bladed Lobster":
-                    GameObject.Instantiate(IRFANPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 89;
-					card.brawl = 168;
-					card.cunning = 82;
-					card.idealPressure = 3;
-                    break;
-                case "Danilo the Piercing Tatui":
-                    GameObject.Instantiate(DANILOPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 111;
-					card.brawl = 151;
-					card.cunning = 75;
-					card.idealPressure = 3;
-                    break;
-                case "Ze Pereira the Sandbar Shark":
-                    GameObject.Instantiate(ZEPEREIRAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 85;
-					card.brawl = 170;
-					card.cunning = 85;
-					card.idealPressure = 1;
-                    break;
-                case "Bob Saw the Anchovy":
-                    GameObject.Instantiate(BOB_SAWPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 154;
-					card.brawl = 90;
-					card.cunning = 80;
-					card.idealPressure = 2;
-                    break;
-                case "Tisya the Spotted Blademaster":
-                    GameObject.Instantiate(TISYAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 100;
-					card.brawl = 90;
-					card.cunning = 130;
-					card.idealPressure = 2;
-                    break;
-                case "Sasha the Wrestling Beluga":
-                    GameObject.Instantiate(SASHAPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 99;
-					card.brawl = 165;
-					card.cunning = 75;
-					card.idealPressure = 1;
-                    break;
-                case "Medhansh the Swordsman":
-                    GameObject.Instantiate(MEDHANSHPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 65;
-					card.brawl = 135;
-					card.cunning = 130;
-					card.idealPressure = 2;
-                    break;
-                case "Sirilo the Marbled Swim Crab":
-                    GameObject.Instantiate(SIRILOPref).transform.SetParent(myTeamContent.transform, false);
-                    card.agility = 93;
-                    card.brawl = 140;
-                    card.cunning = 100;
-                    card.idealPressure = 3;
-                    break;
-                case "Mahesh the Slashing Mackerel":
-                    GameObject.Instantiate(MAHESHPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 160;
-					card.brawl = 97;
-					card.cunning = 70;
-					card.idealPressure = 3;
-                    break;
-                case "Richard the Opportunist Cockle":
-                    GameObject.Instantiate(RICHARDPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 98;
-					card.brawl = 163;
-					card.cunning = 80;
-					card.idealPressure = 2;
-                    break;
-                case "Ed the Punk Salmon":
-                    GameObject.Instantiate(EDPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 160;
-					card.brawl = 85;
-					card.cunning = 80;
-					card.idealPressure = 4;
-                    break;
-                case "Adam Risso the Dolphin":
-                    GameObject.Instantiate(ADAMPref).transform.SetParent(myTeamContent.transform, false);
-					card.agility = 140;
-					card.brawl = 75;
-					card.cunning = 105;
-					card.idealPressure = 2;
-                    break;
+            
+                    GameObject.Instantiate(ChampionPref).transform.SetParent(myTeamContent.transform, false);
+            ChampionPref.GetComponentInChildren<Item>().id = card.id;             
+            ChampionPref.GetComponentInChildren<Item>().name = card.name;
+            ChampionPref.GetComponentInChildren<Item>().image = card.image;
+            ChampionPref.GetComponentInChildren<Item>().tribe = card.tribe;
+            ChampionPref.GetComponentInChildren<Item>().rarity = card.rarity;
+            ChampionPref.GetComponentInChildren<Item>().elite = card.elite;
+   //    ChampionPref.GetComponentInChildren<Item>().idealPressure = card.idealPressure;
+            ChampionPref.GetComponentInChildren<Item>().brawl = card.brawl;
+            ChampionPref.GetComponentInChildren<Item>().agility = card.agility;
+            ChampionPref.GetComponentInChildren<Item>().cunning = card.cunning;
 
-                default:
-                    break;
-            }
-        }
+
+}
         try
         {
             GameObject.Find("ScriptHolder").GetComponent<OPENAndEXITGame>().LoadTeams();
--- Assets/Scripts/Team Manager/Item.cs
+++ Assets/Scripts/Team Manager/Item.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +9,30 @@ public class Item : MonoBehaviour
     private GameObject[] slotList;
     [SerializeField]
     private Color slotColor;
-    
+
+    public int id;
+    public string name;
+    public string image;
+    public string tribe;
+    public string rarity;
+    public bool elite;
+    public int idealPressure;
+    public float brawl;
+    public float agility;
+    public float cunning;
+    public float BattlePower;
+
+    public Sprite JOHN_FANGO, CAPTAIN,Ted, NENRUNG, REZAUL, GARRINCHA, LUKE, RAY, SIR_WESTLEY, LIZA, HANIS, SARGEANT_HARTMAN, RIDLEY, ARSELLA,
+    ROCUS, AARON, WANDERLEY, LOLA, TODD, JOHNNY, SOORYA, IRFAN, DANILO, ZEPEREIRA, BOB_SAW, TISYA, SASHA, MEDHANSH, SIRILO, MAHESH,
+    RICHARD, ED, ADAM;
+
+
     void Start()
     {
         championObj = GameObject.Find("ScriptHolder").GetComponent<CardConstructor>().championObj;
+        CardImage();
+        // Battle_Power = A+(0.8*B)+(1.2*C)
+        BattlePower = agility + (0.8f * brawl) + (1.2f * cunning);
     }
     
     public void OnMouseDown()
@@ -68,5 +86,154 @@ public class Item : MonoBehaviour
             gameObject.GetComponent<Image>().color = new Color(1, 1, 1,1);   
         }
     }
+     
+public void CardImage()
+{
+        switch (name)
+        {
+            case "John Fango the Tactical Megalodon":
+                this.GetComponent<Image>().sprite= JOHN_FANGO;
+               idealPressure = 4;
+                break;
+            case "Captain Costa the Man O'War":
+                this.GetComponent<Image>().sprite = CAPTAIN;
 
+                idealPressure = 1;
+                break;
+            case "Ted Righty the Boxer Whale":
+                this.GetComponent<Image>().sprite = Ted;
+               
+                idealPressure = 2;
+                break;
+            case "Nenrung the Dragon":
+                this.GetComponent<Image>().sprite = NENRUNG;
+                
+                idealPressure = 3;
+                break;
+            case "Rezaul the master Octopus":
+                this.GetComponent<Image>().sprite = REZAUL;
+               idealPressure = 1;
+                break;
+            case "Garrincha the Tough Blue Lobster":
+                this.GetComponent<Image>().sprite = GARRINCHA;
+                idealPressure = 2;
+                break;
+            case "Luke the Turtle":
+                this.GetComponent<Image>().sprite = LUKE;
+                idealPressure = 2;
+                break;
+            case "Ray the Whipping Manta":
+                this.GetComponent<Image>().sprite = RAY;
+                idealPressure = 3;
+                break;
+            case "Sir Westley the Fencing Dolphin":
+                this.GetComponent<Image>().sprite = SIR_WESTLEY;
+                idealPressure = 1;
+                break;
+            case "Liza the Carrier Crab":
+                this.GetComponent<Image>().sprite = LIZA;
+                idealPressure = 3;
+                break;
+            case "Hanis the Menacing Flatfish":
+                this.GetComponent<Image>().sprite = HANIS;
+                idealPressure = 2;
+                break;
+            case "Sargeant Hartman the Navy Seal":
+                this.GetComponent<Image>().sprite = SARGEANT_HARTMAN;
+                idealPressure = 1;
+                break;
+            case "Ridley the Olive Turtle":
+                this.GetComponent<Image>().sprite = RIDLEY;
+                idealPressure = 1;
+                break;
+            case "Arsella the Sea Nettle":
+                this.GetComponent<Image>().sprite = ARSELLA;
+                idealPressure = 4;
+                break;
+            case "Rocus 'Shades' the Blue Shark":
+                this.GetComponent<Image>().sprite = ROCUS;
+                idealPressure = 2;
+                break;
+            case "Aaron the Ramming Crab": 
+                this.GetComponent<Image>().sprite = AARON;
+                idealPressure = 3;
+                break;
+            case "Wanderley the Grappling Octopus":
+                this.GetComponent<Image>().sprite = WANDERLEY;
+                idealPressure = 1;
+                break;
+            case "Lola the Buff Blowfish":
+                this.GetComponent<Image>().sprite = LOLA;
+                idealPressure = 3;
+                break;
+            case "Todd the Barber Eel":
+                this.GetComponent<Image>().sprite = TODD;
+                idealPressure = 4;
+                break;
+            case "Jhonny the Sailor Squid":
+                this.GetComponent<Image>().sprite = JOHNNY;
+                idealPressure = 2;
+                break;
+            case "Soorya the Needling Box Jellyfish":
+                this.GetComponent<Image>().sprite = SOORYA;
+                idealPressure = 4;
+                break;
+            case "Irfan the Bladed Lobster":
+                this.GetComponent<Image>().sprite = IRFAN;
+                idealPressure = 3;
+                break;
+            case "Danilo the Piercing Tatui":
+                this.GetComponent<Image>().sprite = DANILO;
+                idealPressure = 3;
+                break;
+            case "Ze Pereira the Sandbar Shark":
+                this.GetComponent<Image>().sprite = ZEPEREIRA;
+                idealPressure = 1;
+                break;
+            case "Bob Saw the Anchovy":
+                this.GetComponent<Image>().sprite = BOB_SAW;
+                idealPressure = 2;
+                break;
+            case "Tisya the Spotted Blademaster":
+                this.GetComponent<Image>().sprite = TISYA;
+                idealPressure = 2;
+                break;
+            case "Sasha the Wrestling Beluga":
+                this.GetComponent<Image>().sprite = SASHA;
+                idealPressure = 1;
+                break;
+            case "Medhansh the Swordsman":
+                this.GetComponent<Image>().sprite = MEDHANSH;
+                idealPressure = 2;
+                break;
+            case "Sirilo the Marbled Swim Crab":
+                this.GetComponent<Image>().sprite = SIRILO;
+                idealPressure = 3;
+                break;
+            case "Mahesh the Slashing Mackerel":
+                this.GetComponent<Image>().sprite = MAHESH;
+                idealPressure = 3;
+                break;
+            case "Richard the Opportunist Cockle":
+                this.GetComponent<Image>().sprite = RICHARD;
+                idealPressure = 2;
+                break;
+            case "Ed the Punk Salmon":
+                this.GetComponent<Image>().sprite = ED;
+                idealPressure = 4;
+                break;
+            case "Adam Risso the Dolphin":
+                this.GetComponent<Image>().sprite = ADAM;
+                idealPressure = 2;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+  
+   
 }
+
+
